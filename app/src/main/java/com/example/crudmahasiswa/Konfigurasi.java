9953f462535a3a189e58686cb66d6f01a0759e0e
rezalampe <rezafebryanl@gmail.com>
2020-05-15 06:50:04 +0700

    modul 6

---
+++ app/src/main/java/com/example/crudmahasiswa/Konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.crudmahasiswa;
+
+public class Konfigurasi {
+    public static final String URL_ADD="https://rezalampe.000webhostapp.com/insert.php";
+    public static final String URL_GET_ALL="https://rezalampe.000webhostapp.com/read.php";
+    public static final String URL_GET_MHS="https://rezalampe.000webhostapp.com/select.php";
+    public static final String URL_UPDATE_MHS="https://rezalampe.000webhostapp.com/update.php";
+    public static final String URL_DELETE_MHS="https://rezalampe.000webhostapp.com/delete.php";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID = "mhs_id";
+}
